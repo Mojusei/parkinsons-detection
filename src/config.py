cabcd1333f124d3f4165067d81650a5a632edfcc
Mojusei <metra9991@mail.ru>
2025-11-12 23:50:05 +0400

    EDA и визуализация

--- src/config.py
+++
@@ -1,6 +0,0 @@
-from pathlib import Path
-
-
-ROOT_DIR = Path(__file__).resolve().parent 
-DATA_PATH = ROOT_DIR / "data" / "raw" / "parkinsons.data"
-MODEL_PATH = ROOT_DIR / "models" / "model.joblib"
